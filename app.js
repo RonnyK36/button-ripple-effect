96b7bb0802c2ed67a20baabb4bac0cd791ac3170
RonnyK36 <ronnykelvyne3@gmail.com>
2022-08-14 20:31:24 +0300

    Added a function to get mouse position and start animation

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("Hello");
+const btnEl = document.querySelector('.btn');
+
+btnEl.addEventListener('mouseover', (event) => {
+    const xPos = (event.pageX - btnEl.offsetLeft);
+    const yPos = (event.pageY - btnEl.offsetTop);
+
+    btnEl.style.setProperty("--xPosition", xPos + "px");
+    btnEl.style.setProperty("--yPosition", yPos + "px");
+})
